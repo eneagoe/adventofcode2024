0dc3db05c1f322cb827ac8432531c8a2e3aee970
eugen neagoe <eneagoe@gmail.com>
2024-12-01 16:31:25 +0200

    Add ruby solution for 2nd puzzle of 1st day

--- day1.rb
+++ day1.rb
@@ -5,3 +5,13 @@ first = File.open(ARGV[0]).map do
 end.transpose.map(&:sort).reduce(&:zip).sum { |(a, b)| (a - b).abs }
 
 puts first
+
+locations, appearances = File.open(ARGV[0]).map do
+  _1.split(/\s+/).map(&:to_i)
+end.transpose
+
+frequencies = appearances.each_with_object(Hash.new(0)) do |location, h|
+  h[location] += 1
+end
+
+puts locations.sum { _1 * frequencies[_1] }
