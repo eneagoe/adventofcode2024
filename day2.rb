33b05480ec3cd7e800236a5955aac72bc5cf5a2a
eugen neagoe <eneagoe@gmail.com>
2024-12-02 23:11:42 +0200

    Add ruby solution for 2nd puzzle of 2nd day

--- day2.rb
+++ day2.rb
@@ -1,13 +1,26 @@
 #!/usr/bin/env ruby -w
 
+def valid?(arr)
+  dir = arr[0] < arr[1]
+
+  arr.each_cons(2).all? { |(a, b)| (a < b) == dir && (a - b).abs.between?(1, 3) }
+end
+
 levels = File.open(ARGV[0]).map do
   _1.split(/\s+/).map(&:to_i)
 end
 
 first = levels.count do |level|
-  dir = level[0] < level[1]
-
-  level.each_cons(2).all? { |(a, b)| (a < b) == dir && (a - b).abs.between?(1, 3) }
+  valid?(level)
 end
 
 puts first
+
+second = levels.count do |level|
+  # brute-force it, input size is small
+  valid?(level) || (0...level.size).any? do |i|
+    valid?(level[...i] + level[i + 1..])
+  end
+end
+
+puts second
