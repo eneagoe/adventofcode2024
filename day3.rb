4cb7da8a136d12faa4f1008405a54f8b148477fc
eugen neagoe <eneagoe@gmail.com>
2024-12-03 23:25:03 +0200

    Add ruby solution for 2nd puzzle of 3rd day

--- day3.rb
+++ day3.rb
@@ -8,3 +8,17 @@ File.open(ARGV[0]).readlines.each do |line|
 end
 
 puts first
+
+second = 0
+doing = true
+File.open(ARGV[0]).readlines.each do |line|
+  line.scan(/(do\(\)|don't\(\))|mul\((\d{1,3}),(\d{1,3})\)/).each do |m, a, b|
+    if m
+      doing = m == 'do()'
+    elsif a && b
+      second += a.to_i * b.to_i if doing
+    end
+  end
+end
+
+puts second
