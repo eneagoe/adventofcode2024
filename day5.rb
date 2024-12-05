792cb6d07cf9fb36a3c3c24ec83ef63ca47be7f7
eugen neagoe <eneagoe@gmail.com>
2024-12-05 22:43:51 +0200

    Add ruby solution for 2nd puzzle of 5th day

--- day5.rb
+++ day5.rb
@@ -2,6 +2,7 @@
 
 g = Hash.new { |hash, key| hash[key] = [] }
 first = 0
+second = 0
 building = true
 
 File.open(ARGV[0]).readlines.each do |line|
@@ -21,8 +22,28 @@ File.open(ARGV[0]).readlines.each do |line|
 
     valid = (0...pages.size).all? { |i| !pages[..i].intersect?(g[pages[i]]) }
 
-    first += pages[pages.size / 2] if valid
+    if valid
+      first += pages[pages.size / 2]
+      next
+    end
+
+    # put the pages in the right order
+    current = []
+
+    (0...pages.size).each do |i|
+      if !pages[..i].intersect?(g[pages[i]])
+        current << pages[i]
+      else
+        j = current.size
+        # insert into right position
+        j -= 1 while current[...j].intersect?(g[pages[i]])
+        current.insert(j, pages[i])
+      end
+    end
+
+    second += current[current.size / 2]
   end
 end
 
 puts first
+puts second
